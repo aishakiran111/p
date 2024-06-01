18bc95e92f45b0d4a1d72e50decba9db17f6b950
aishakiran111 <aishakiran111@gmail.com>
2024-06-01 21:20:25 +0500

    first

---
+++ first/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Aisha Kiran"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Text("Aisha Kiran"),
+            Text("dfffffffffff"),
+            Text("Aisha Kiran"),
+            Text("dfffffffffff")
+          ],
+        ),
+      ),
+    ));
+  }
+}
